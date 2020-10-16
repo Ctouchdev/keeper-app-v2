e81bfd36728e683f39604e025681d22f63124c9b
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-16 13:14:43 -0500

    Updates

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import Notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {Notes.map((allNotes) => (
+        <Note title={allNotes.title} content={allNotes.content} />
+      ))}
       <Footer />
     </div>
   );
